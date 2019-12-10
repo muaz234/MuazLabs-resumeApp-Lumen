e1a9e9edc939808f978b2a329c1959adbe4674ad
muaz234 <ahmedmuaz0152@gmail.com>
2019-12-10 17:01:31 +0800

    update route

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,8 @@ $router->group(['prefix' => 'candidate'], function () use ($router) //serve cand
     $router->post('/', 'CandidateController@add');
     $router->put('/{id}', 'CandidateController@edit');
     $router->delete('/{id}', 'CandidateController@delete');
+
+
 });
 
 $router->group(['prefix' => 'education'], function () use ($router) //serve education prefix
@@ -29,4 +31,39 @@ $router->group(['prefix' => 'education'], function () use ($router) //serve educ
     $router->delete('/{candidateId}/{id}', 'EducationHistoryController@delete');
 });
 
+$router->group(['prefix' => 'reference'], function () use ($router) //serve reference prefix
+{
+    $router->get('/', 'ReferenceController@index');
+    $router->get('/{candidateId}', 'ReferenceController@show');
+    $router->post('/', 'ReferenceController@add');
+    $router->put('/{candidateId}/{id}', 'ReferenceController@edit');
+    $router->delete('/{candidateId}/{id}', 'ReferenceController@delete');
+});
 
+$router->group(['prefix' => 'portfolio'], function () use ($router) //serve portfolio prefix
+{
+    $router->get('/', 'PortfolioController@index');
+    $router->get('/{candidateId}', 'PortfolioController@show');
+    $router->post('/', 'PortfolioController@add');
+    $router->put('/{candidateId}/{id}', 'PortfolioController@edit');
+    $router->delete('/{candidateId}/{id}', 'PortfolioController@delete');
+});
+
+
+$router->group(['prefix' => 'publication'], function () use ($router) //serve publication prefix
+{
+    $router->get('/', 'PublicationController@index');
+    $router->get('/{candidateId}', 'PublicationController@show');
+    $router->post('/', 'PublicationController@add');
+    $router->put('/{candidateId}/{id}', 'PublicationController@edit');
+    $router->delete('/{candidateId}/{id}', 'PublicationController@delete');
+});
+
+$router->group(['prefix' => 'work_experience'], function () use ($router) //serve publication prefix
+{
+    $router->get('/', 'WorkExperienceController@index');
+    $router->get('/{candidateId}', 'WorkExperienceController@show');
+    $router->post('/', 'WorkExperienceController@add');
+    $router->put('/{candidateId}/{id}', 'WorkExperienceController@edit');
+    $router->delete('/{candidateId}/{id}', 'WorkExperienceController@delete');
+});
