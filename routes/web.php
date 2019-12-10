a1c21684b7b38d9d61e1d2998ff4583f5d2771c5
muaz234 <ahmedmuaz0152@gmail.com>
2019-12-10 21:31:10 +0800

    update routes

--- routes/web.php
+++ routes/web.php
@@ -59,11 +59,29 @@ $router->group(['prefix' => 'publication'], function () use ($router) //serve pu
     $router->delete('/{candidateId}/{id}', 'PublicationController@delete');
 });
 
-$router->group(['prefix' => 'work_experience'], function () use ($router) //serve publication prefix
+$router->group(['prefix' => 'work_experience'], function () use ($router) //serve work_exp prefix
 {
     $router->get('/', 'WorkExperienceController@index');
     $router->get('/{candidateId}', 'WorkExperienceController@show');
     $router->post('/', 'WorkExperienceController@add');
     $router->put('/{candidateId}/{id}', 'WorkExperienceController@edit');
     $router->delete('/{candidateId}/{id}', 'WorkExperienceController@delete');
+});
+
+$router->group(['prefix' => 'skill'], function () use ($router) //serve certificate prefix
+{
+    $router->get('/', 'SkillController@index');
+    $router->get('/{candidateId}', 'SkillController@show');
+    $router->post('/', 'SkillController@add');
+    $router->put('/{candidateId}/{id}', 'SkillController@edit');
+    $router->delete('/{candidateId}/{id}', 'SkillController@delete');
+});
+
+$router->group(['prefix' => 'certificate'], function () use ($router) //serve certificate prefix
+{
+    $router->get('/', 'CertificateController@index');
+    $router->get('/{candidateId}', 'CertificateController@show');
+    $router->post('/', 'CertificateController@add');
+    $router->put('/{candidateId}/{id}', 'CertificateController@edit');
+    $router->delete('/{candidateId}/{id}', 'CertificateController@delete');
 });
